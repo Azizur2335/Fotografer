e3a136ab9fa9b274eb853299b8a74bffe65e30c1
Azizur2335 <azizurrifki2335@gmail.com>
2025-09-23 00:51:06 +0800

    merge css signin dan signup

--- SignIn.php
+++ SignIn.php
@@ -8,24 +8,24 @@
     <div class="kiri-si">
         <div class="logo-si">Logo</div>
         <div class="login-box-si">
-            <h1 style="text-align: center; margin-bottom: 10px;">Log In</h1>
-            <p style="font-size: 14px; text-align: center; margin-bottom: 30px;">Lorem ipsum dolor sit amet adipiscing elit.</p>
-            <form action="backend/proses_login.php" method="POST" onsubmit="return validasiLogin()">
+            <h1>Log In</h1>
+            <p>Lorem ipsum dolor sit amet adipiscing elit.</p>
+            <form class="form-si" action="backend/proses_login.php" method="POST" onsubmit="return validasiLogin()">
                 <label>Email*</label>
                 <input id="email" type="email" name="email">
                 <div class="label-row-si">
                     <label>Password*</label>
-                    <p class="forgor-si">Forgot your password?</p>
+                    <span class="forgor-si"><a href="#">Forgot your password?</a></span>
                 </div>
                 <input id="password" type="password" name="password">
                 <button class="btn-si btn-login">Log in</button>
                 <button class="btn-si btn-google">Log in with Google</button>
-                <p style="font-size: 12px; text-align: center;">
+                <p class="ga-punya-akun">
                     Don't have an account? <a href="SignUp.php">Sign Up</a>
                 </p>
             </form>
         </div>
-        <div style="font-size: 10px;">© 2025 NamaKelompok</div>
+        <div class="footer-si">© 2025 NamaKelompok</div>
     </div>
     <div class="kanan-si">
         <img src="./Assets/cewegwpt2.jpg">
--- SignUp.php
+++ SignUp.php
@@ -1,105 +1,30 @@
 <!DOCTYPE html>
 <head>
     <title>Sign Up</title>
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            align-items: center;
-            min-height: 100vh;
-            background-color: #fff;
-        }
-        .logo-su {
-            font-weight: bold;
-            font-size: 28px;
-            text-align: center;
-            margin-top: 20px;
-        }
-        .box-container {
-            flex: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-        }
-        .box-su {
-            border: 1px solid black;
-            padding: 30px 40px;
-            background-color: white;
-            max-width: 400px;
-            width: 100%;
-            text-align: center;
-            min-height: 540px;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-        }
-        form label {
-            font-size: 12px;
-            display: inline-block;
-            text-align: left;
-            width: 100%;
-        }
-        form input {
-            width: 100%;
-            border: solid 1px;
-            font-size: 14px;
-            padding: 10px;
-            margin-bottom: 20px;
-        }
-        .btn-su {
-            width: 100%;
-            height: 35px;
-            font-size: 12px;
-            margin-bottom: 15px;
-            border-radius: 2px;
-            cursor: pointer;
-        }
-        .btn-login {
-            background-color: black;
-            color: white;
-            border: none;
-        }
-        .btn-google {
-            background-color: white;
-            border: solid 1px;
-        }
-        .footer {
-            font-size: 10px;
-            text-align: center;
-            margin-bottom: 20px; /* jarak bawah */
-        }
-    </style>
+    <link rel="stylesheet" href="Assets/css/style.css">
 </head>
-<body>
+<body class="body-su">
     <div class="logo-su">Logo</div>
-    <div class="box-container">
+    <div class="box-container-su">
         <div class="box-su">
             <h1 style="text-align: center; margin-top: 20px; font-size: 35px;">Sign Up</h1>
             <p style="font-size: 15px; text-align: center; margin-bottom: 5px;">Lorem ipsum dolor sit amet adipiscing elit.</p>
-            <form id="formRegist" action="backend/proses_registrasi.php" method="POST" onsubmit="return validasiForm()">
+            <form class="form-su" id="formRegist" action="backend/proses_registrasi.php" method="POST" onsubmit="return validasiForm()">
                 <label for="nama">Name*</label>
                 <input id="nama" name="nama" type="text">
                 <label for="email">Email*</label>
                 <input id="email" name="email" type="email">
                 <label for="password">Password*</label>
                 <input id="password" name="password" type="password">
-                <button class="btn-su btn-login">Sign up</button>
-                <button class="btn-su btn-google">Sign up with Google</button>
+                <button class="btn-su btn-signup">Sign up</button>
+                <button class="btn-su btn-sugoogle">Sign up with Google</button>
             </form>
             <p style="font-size: 12px; text-align: center;">
                 Already have an account? <a href="SignIn.php">Log In</a>
             </p>
         </div>
     </div>
-    <div class="footer">© 2025 NamaKelompok</div>
+    <div class="footer-su">© 2025 NamaKelompok</div>
     <script src="form.js"></script>
 </body>
 </html>
